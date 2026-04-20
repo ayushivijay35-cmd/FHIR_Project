826259ed6536eba9c527fadbfd7adfed7c4530ba
ayushivijay35-cmd <ayushivijay35@gmail.com>
2026-04-20 13:03:33 +0530

    Update config.py

--- FHIR-Project/config/config.py
+++ FHIR-Project/config/config.py
@@ -5,5 +5,5 @@ config_path = "/Volumes/workspace/default/fhir_lakehouse/config/config.json"
 with open(config_path, "r") as f:
     config = json.load(f)
 
-print("✅ Loaded config:", config["api"]["base_url"])
+print(" Loaded config:", config["api"]["base_url"])
 
